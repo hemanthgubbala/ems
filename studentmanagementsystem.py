8420df973cfbaec375c6f39fbcabd9c5ef3de3de
hemanthgubbala <130303965+hemanthgubbala@users.noreply.github.com>
2024-07-15 00:32:01 +0530

    Update studentmanagementsystem.py

--- studentmanagementsystem.py
+++ studentmanagementsystem.py
@@ -6,7 +6,7 @@ con = mysql.connector.connect(
     host="localhost",
     user="root",
     password="password",
-    database="school"
+    database="student_management"
 )
 
 cursor = con.cursor()
